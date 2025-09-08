4ca7c6d03944421c4cce5f54e9083099d1bbe56c
stripodi <simone.tripodi@terradue.com>
2025-09-08 11:45:13 +0200

    feat: updating the input/output schema generation in order to support byReference/byValue for Directory/File

--- src/cwl2ogc/__about__.py
+++ src/cwl2ogc/__about__.py
@@ -10,4 +10,4 @@ You should have received a copy of the license along with this work.
 If not, see <https://creativecommons.org/licenses/by-sa/4.0/>.
 """
 
-__version__ = "0.9.0"
+__version__ = "0.10.0.dev1"
